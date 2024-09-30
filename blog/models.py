b86eacb32d57704abde9fb2e15560c0222607017
Kushal Dotel <kushal.dotel@study.lbef.edu.np>
2024-09-30 09:15:32 +0545

    update the blog

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.utils.text import slugify
+from django_ckeditor_5.fields import CKEditor5Field
+
 # Create your models here.
 class Blog(models.Model):
 
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    short_description = models.TextField(null=True, blank=True)
+    description = CKEditor5Field('Description', config_name='default')
     banner_image = models.ImageField(upload_to='blogs/',null=True,blank=True)
 
     slug = models.SlugField(unique=True,null=True, blank=True)
